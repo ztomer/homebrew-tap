6bb96966a3c51d94219f564481a02f292e160c62
Tomer Zaidentein <ztomer@gmail.com>
2026-09-07 21:43:43 -0400

    divoom-control 0.34.0

--- Casks/divoom-control.rb
+++ Casks/divoom-control.rb
@@ -1,6 +1,6 @@
 cask "divoom-control" do
-  version "0.33.0"
-  sha256 "c33a78538d24d0b90d741673e9d185bfa93d668e2e4a80416dad68fd3695e5c9"
+  version "0.34.0"
+  sha256 "666f047be5e36c2f0fe7f6f6191c1c819a063104521d96002da955cff0059808"
 
   url "https://github.com/ztomer/divoom_control/releases/download/v#{version}/Divoom-v#{version}.dmg"
   name "Divoom Control"
